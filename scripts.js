c3c296000328b85f379c123ee655cd87d0a27128
David-Hebert <king.david.abear@zoho.com>
2020-11-22 21:27:48 -0600

    lodash practice

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+//const _ = require('lodash')
+
+const arrayOfNums = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(_.chunk(arrayOfNums, 3));
+
+console.log(_.reverse(arrayOfNums));
+
+console.log(_.without(arrayOfNums, 2, 5));
+
+console.log(_.shuffle(arrayOfNums));
